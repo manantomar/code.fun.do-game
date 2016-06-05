11c62d26933867346091c3e937ef79ddfcd66aec
manantomar <manan.tomar@gmail.com>
2016-06-05 19:07:09 +0530

    initial commit
    made the basic platform and added rotation script to it on click down of
    mouse

---
+++ game_test/Assets/rotate.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	// Use this for initialization
+	float xDeg;
+	float yDeg;
+	Quaternion fromRotation;
+	Quaternion toRotation;
+	float lerpSpeed = 2.0f;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0)&&(transform.rotation. <= 45)&&(transform.rotation.eulerAngles.y <= 45)) {
+			
+			xDeg -= Input.GetAxis ("Mouse X");
+			yDeg += Input.GetAxis ("Mouse Y");
+			fromRotation = transform.rotation;
+			toRotation = Quaternion.Euler(yDeg,xDeg,0);
+			transform.rotation = Quaternion.Lerp(fromRotation,toRotation,Time.deltaTime  * lerpSpeed);
+		}
+
+	}
+		
+		
+}
